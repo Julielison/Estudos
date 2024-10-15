a388506c914cf93e98f3a7859b5ed0ee450588bf
Julielison Lima <127698034+Julielison@users.noreply.github.com>
2024-10-14 21:52:47 -0300

    add: documentação com Swagger e OpenAPI

--- Autodidata/5. Java - Bootcamp/JAVA WORKSPACE/my-first-web-api/src/main/java/dio/my_first_web_api/MyFirstWebApiApplication.java
+++ Autodidata/5. Java - Bootcamp/JAVA WORKSPACE/my-first-web-api/src/main/java/dio/my_first_web_api/MyFirstWebApiApplication.java
@@ -3,7 +3,12 @@ package dio.my_first_web_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "API desenvolvida para testes do OpenApi"))
+
 public class MyFirstWebApiApplication {
 
 	public static void main(String[] args) {
--- Autodidata/5. Java - Bootcamp/JAVA WORKSPACE/my-first-web-api/src/main/java/dio/my_first_web_api/controller/UsuarioController.java
+++ Autodidata/5. Java - Bootcamp/JAVA WORKSPACE/my-first-web-api/src/main/java/dio/my_first_web_api/controller/UsuarioController.java
@@ -3,41 +3,76 @@ package dio.my_first_web_api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.my_first_web_api.model.Usuario;
 import dio.my_first_web_api.repository.UsuarioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users", produces = {"application/json"})
+@Tag(name = "open-api")
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
 
+    @Operation(summary = "Lista todos os usuários", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de usuários recuperada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro ao recuperar a lista de usuários")
+    })
     @GetMapping()
     public List<Usuario> getUsers(){
         return repository.listAll();
     }
 
+    @Operation(summary = "Obtém um usuário pelo nome de usuário", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário recuperado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao recuperar o usuário")
+    })
     @GetMapping("/{username}")
     public Usuario getOne(@PathVariable("username") String username){
         return repository.findByUsername(username);
     }
 
+    @Operation(summary = "Deleta um usuário pelo ID", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao deletar o usuário")
+    })
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Integer id){
         repository.remove(id);
     }
 
-    @PostMapping()
+    @Operation(summary = "Cria um novo usuário", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados de entrada inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao criar o usuário")
+    })
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void post(@RequestBody Usuario usuario){
         repository.save(usuario);
     }
+    @PutMapping()
+    public void putUser(@RequestBody Usuario usuario){
+        repository.save(usuario);
+    }
+
 
 }
