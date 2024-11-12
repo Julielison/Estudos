acd2894b3efcbda576ea9f7b023f64fbd2e7b20c
Julielison Lima <127698034+Julielison@users.noreply.github.com>
2024-11-11 22:05:32 -0300

    update: spring security

--- Autodidata/5. Java - Bootcamp/JAVA WORKSPACE/dio-spring-security/src/main/java/dio/dio_spring_security/WebSecurityConfig.java
+++ Autodidata/5. Java - Bootcamp/JAVA WORKSPACE/dio-spring-security/src/main/java/dio/dio_spring_security/WebSecurityConfig.java
@@ -1,13 +1,10 @@
 package dio.dio_spring_security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
@@ -15,13 +12,15 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests(requests -> requests
-                .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.POST, "/login").permitAll()
-                .antMatchers("/managers").hasAnyRole("MANAGERS")
-                .antMatchers("/users").hasAnyRole("USERS", "MANAGERS")
-                .anyRequest().authenticated()).httpBasic(withDefaults());
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user")
+            .password("{noop}user123")
+            .roles("USERS")
+            .and()
+            .withUser("admin")
+            .password("{noop}master123")
+            .roles("MANAGERS");
     }
 
 }
